acae3bcdf44296de437e3bf9af69c162e800b520
leonardo <leonardo.vanzin@hotmail.com>
2022-03-04 09:12:46 -0300

    input

---
+++ trilateracao.py
@@ -0,0 +1,7 @@
+
+x1 = int(input("valor de x1"))
+x2 = int(input("valor de x1"))
+x3 = int(input("valor de x1"))
+y1 = int(input("valor de x1"))
+y2 = int(input("valor de x1"))
+y3 = int(input("valor de x1"))
