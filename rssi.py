2f7dc98d583611c6405be41510bb7cc0ddc91536
leonardo <leonardo.vanzin@hotmail.com>
2022-05-05 13:14:17 -0300

    scanner

--- rssi.py
+++ rssi.py
@@ -1,10 +1,11 @@
 import asyncio 
-import bleak
+
 from bleak import BleakScanner
 
 async def main():
     devices = await BleakScanner.discover()
     for d in devices:
-        print(d)
+        if (d.address == '90:E2:02:91:62:09'): 
+            print(d.address, d.rssi)
 
 asyncio.run(main())
