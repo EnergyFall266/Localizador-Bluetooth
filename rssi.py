98fe96099e859f0505ddc90bf20aed03e4493a43
leonardo <leonardo.vanzin@hotmail.com>
2022-06-23 11:47:06 -0300

    busca

--- rssi.py
+++ rssi.py
@@ -1,19 +1,41 @@
 import asyncio
-import time 
 
 from bleak import BleakScanner
 
+# with open('coleta.csv', 'a') as f:
+#     f.write('Address,RSSI\n')    
+
 async def main():
-    # start=time.time()
-    devices = await BleakScanner.discover(timeout=3)
+    global a,b,c    
+    devices = await BleakScanner.discover(timeout=4)
     for d in devices:
-        if (d.address == '90:E2:02:91:62:09' or d.address == '3C:A3:08:96:68:60' or d.address == '4C:24:98:5D:05:A3'): 
-            print(d.rssi)
-    # print(time.time()-start)
-for i in range(0,50):
+        if (d.address == '90:E2:02:91:62:09' and a<5): 
+            with open('coleta.csv', 'a') as f:
+                f.write(f'{d.address},{d.rssi}\n')
+            
+            # print(d.address ,d.rssi)
+            # print(a)
+            a+=1
+        if (d.address == '3C:A3:08:96:68:60' and b<5):
+            with open('coleta.csv', 'a') as f:
+                f.write(f'{d.address},{d.rssi}\n')
+            # print(d.address ,d.rssi)
+            # print(b)
+            b+=1
+        if (d.address == '4C:24:98:5D:05:A3' and c<5):
+            with open('coleta.csv', 'a') as f:
+                f.write(f'{d.address},{d.rssi}\n')
+            # print(d.address ,d.rssi)
+            # print(c)
+            c+=1
+a=0
+b=0
+c=0
+for i in range(0,2050):
     
     asyncio.run(main())
-    
+    if(a==5 and b==5 and c==5):
+        break
 
 
     
