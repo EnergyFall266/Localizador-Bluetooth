0e472397e3a76f3446dc4bef0444c48d8f051fe7
leonardo <leonardo.vanzin@hotmail.com>
2022-07-01 17:29:11 -0300

    hyperparametros coord

--- odena.py
+++ odena.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 
-df = pd.read_csv('results21.csv')
+df = pd.read_csv('results41.csv')
 
 df=df.sort_values(by=['mse'])
 
-df.to_csv('resultsOrd21.csv',index=False)
+df.to_csv('resultsOrd41.csv',index=False)
